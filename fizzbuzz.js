50e985032da92f13961bc8b92e69cb920f0e0077
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-24 16:24:20 -0600

    BAM!!

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,4 @@
-(function b(arr,n,i,idx,res) {
-    idx>=arr.length?console.log(res===''?i:res):(function(){res+=!!(i%arr[idx])?"":arr[idx+1];b(arr,n,i,idx+2,res);!!!idx&&i<n?b(arr,n,i+1,0,0,''):null;})();
-})([3,'fizz',5,'buzz'],100,0,0,'');
+function b(arr,n,i,idx,res) {
+    idx<arr.length?b(arr,n,i,idx+2,res+(i%arr[idx]?'':arr[idx+1]))||!idx&&i<n?b(arr,n,i+1,0,''):0:console.log(res?res:i);
+}
+b([3, 'fizz', 5, 'buzz'], 100,0,0,'')
