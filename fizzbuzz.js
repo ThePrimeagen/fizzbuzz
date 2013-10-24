9b25fbe63f7a1e0d93b399e70bcf7e4072178c27
michaelpaulson-wf <michael.paulson@webfilings.com>
2013-10-24 15:49:35 -0600

    Shortened that crap up.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,3 @@
 (function b(arr,n,i,idx,res) {
-    i=i||0,idx=idx||0,res=res||"";
-    idx>=arr.length?console.log(res===""?i:res):(function(){res+=!!(i%arr[idx])?"":arr[idx+1];b(arr,n,i,idx+2,res);!!!idx&&i<n?b(arr,n,i+1):null;})();
-})([3, 'fizz', 5, 'buzz'], 100);
+    idx>=arr.length?console.log(res===''?i:res):(function(){res+=!!(i%arr[idx])?"":arr[idx+1];b(arr,n,i,idx+2,res);!!!idx&&i<n?b(arr,n,i+1,0,0,''):null;})();
+})([3,'fizz',5,'buzz'],100,0,0,'');
