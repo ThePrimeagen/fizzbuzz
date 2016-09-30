700833378398776f0fbd7784f82f32db7ab17f64
Michael Paulson <michael.b.paulson@gmail.com>
2016-09-30 09:37:56 -0700

    feat(1char): Removed 1 character.

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,3 @@
 !function b(a,n,i,x,r){
-x<a.length?b(a,n,i,x+2,r+(i%a[x]?'':a[x+1]))||!x&&i<n?b(a,n,i+1,0,''):0:console.log(r?r:i)
+x<a.length?b(a,n,i,x+2,r+(i%a[x]?'':a[x+1]))||!x&&i<n?b(a,n,i+1,0,''):0:console.log(r||i)
 }([3,'fizz',5,'buzz'],100,0,0,'')
